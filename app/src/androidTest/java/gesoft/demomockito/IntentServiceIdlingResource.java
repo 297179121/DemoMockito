0f5a9f6b3615cda44e454a932636122bc6b6c884
297179121 <y.h.r@163.com>
2016-07-21 11:54:05 +0800

    IdlingResource使用实例

---
+++ app/src/androidTest/java/gesoft/demomockito/IntentServiceIdlingResource.java
@@ -0,0 +1,46 @@
+package gesoft.demomockito;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.support.test.espresso.IdlingResource;
+
+/**
+ * IdlingResource接口实现espresso对app异步测试
+ */
+public class IntentServiceIdlingResource implements IdlingResource {
+  private final Context context;
+  private IdlingResource.ResourceCallback resourceCallback;
+
+  public IntentServiceIdlingResource(Context context) {
+    this.context = context;
+  }
+
+  @Override
+  public String getName() {
+    return IntentServiceIdlingResource.class.getName();
+  }
+
+  @Override
+  public boolean isIdleNow() {
+    boolean idle = !isIntentServiceRunning();
+    if (idle && resourceCallback != null) {
+      resourceCallback.onTransitionToIdle();
+    }
+    return idle;
+  }
+
+  @Override
+  public void registerIdleTransitionCallback(ResourceCallback resourceCallback) {
+    this.resourceCallback = resourceCallback;
+  }
+
+  private boolean isIntentServiceRunning() {
+    ActivityManager manager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+    for (ActivityManager.RunningServiceInfo info : manager.getRunningServices(Integer.MAX_VALUE)) {
+      if (RepeatService.class.getName().equals(info.service.getClassName())) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
--- app/src/main/java/gesoft/demomockito/MainActivity.java
+++ app/src/main/java/gesoft/demomockito/MainActivity.java
@@ -1,13 +1,55 @@
 package gesoft.demomockito;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+  private TextView input;
+  private TextView output;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+  private LocalBroadcastManager manager;
+  private BroadcastReceiver receiver;
+  private IntentFilter filter;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    input = (TextView) findViewById(R.id.input);
+    output = (TextView) findViewById(R.id.output);
+
+    manager = LocalBroadcastManager.getInstance(this);
+    receiver = new BroadcastReceiver() {
+      @Override
+      public void onReceive(Context context, Intent intent) {
+        output.setText(intent.getStringExtra(RepeatService.KEY_OUTPUT));
+      }
+    };
+    filter = new IntentFilter(RepeatService.INTENT_REPEAT);
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    manager.registerReceiver(receiver, filter);
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+    manager.unregisterReceiver(receiver);
+  }
+
+  public void repeat(View v) {
+    Intent intent = new Intent(this, RepeatService.class);
+    intent.putExtra(RepeatService.KEY_INPUT, input.getText().toString());
+    startService(intent);
+  }
 }
